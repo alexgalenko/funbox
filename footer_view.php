4b1a79950049ad7f978ce3ee0be26f70e02c9dc4
Galenko Alex <alex.galenko@behindtheocean.com>
2012-06-24 19:24:15 +0300

    Create view-file

---
+++ footer_view.php
@@ -0,0 +1,19 @@
+	<div class="navbar navbar-fixed-bottom">
+		<div class="navbar-inner">
+			<div class="container">
+				<ul class="nav">
+					<li><a href="<?=site_url();?>">Funbox</a></li>
+				</ul>
+				<ul class="nav pull-right">
+					<li><a href="#">&copy; 2012, Funbox Pty Ltd</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="/js/jquery1.7.2.js"></script>
+	<script type="text/javascript" src="/js/bootstrap-dropdown.js"></script>
+	<script type="text/javascript" src="/js/jquery-ui-1.7.3.custom.min.js"></script>
+	<script type="text/javascript" src="/js/datepicker.js"></script>
+	<script type="text/javascript" src="/js/pagination.js"></script>
+  </body>
+</html>
